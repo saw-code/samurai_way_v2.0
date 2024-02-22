e6e0dcc0b2ae10106f73425ab1149d968e4cbbe7
Dmitry Pilavskiy <saw.coding@gmail.com>
2024-02-22 19:40:45 +0300

    26_lesson

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,25 +1,7 @@
 import React from 'react';
 import s from './Dialogs.module.css'
-import {NavLink} from 'react-router-dom';
-
-type DialogItemPropsType = {
-  id: number
-  name: string
-}
-
-const DialogItem = (props: DialogItemPropsType) => {
-  return <div className={`${s.dialog} ${s.active}`}>
-    <NavLink to={`/dialogs/${props.id}`}>{props.name}</NavLink>
-  </div>
-}
-
-type MessagePropsType = {
-  message: string
-}
-
-const Message = (props: MessagePropsType) => {
-  return <div className={s.message}>{props.message}</div>
-}
+import {Message} from './Message/Message';
+import {DialogItem} from './DialogItem/DialogItem';
 
 
 export const Dialogs = () => {
